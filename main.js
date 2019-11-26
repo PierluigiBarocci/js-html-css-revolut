b25f9b048b2ab66bd18bcfbba8433b9e9638cd13
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-26 18:24:41 +0100

    Esercizio Terminato Concettualmente - Alla fine la consegna in generale é stata rispettata, credo... con la scusa ho aggiunto un effetto carino tipo il sipario che abbiamo visto stamattina in classe, grazie Sofia

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-$('#closingButton').click(function(){
+// Qui scrivo le condizioni per aprire e chiudere le due parti dell'esercizio
 
+$('#closingButton').click(function(){
     if ($('#close:visible').length > 0) {
         $('#close').slideUp(2000);
         $('#esercizio').slideDown(2000);
@@ -10,3 +11,17 @@ $('#closingButton').click(function(){
         $('#closingButton').html('Vai all\'esercizio');
     }
 });
+
+// il ragionamento è simile a quello del sito a cui si riferisce:
+// Passa sopra un link, apri la sua tendina e anche se te ne vai lascia la tendina aperta, si puó chiudere solo cliccando in qualsiasi altro posto; nel momento in cui invece passo poi sopra un altro link, la tendina precedente si chiude di botto e si apre quella nuova, e via dicendo...
+
+$('.dropdown').mouseenter(function(){
+    if(!$(this).children('.dropdown-list').is(':visible')) {
+        $('.dropdown-list').hide();
+        $(this).children('.dropdown-list').slideDown();
+    }
+});
+
+$('body').click(function(){
+    $('.dropdown-list').slideUp();
+})
